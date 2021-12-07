b9b3db10fa37668ec91368975dc1cbe6856df75f
Amina Hallam <amina.hallam@gmail.com>
2021-12-07 20:49:57 +0100

    Create-Design-of-Web-site

--- API/deleteHoroscope.php
+++ API/deleteHoroscope.php
@@ -1,7 +1,28 @@
 <?php
 
 
+try {
 
+    if($_SERVER["REQUEST_METHOD"]) {
+
+        if($_SERVER["REQUEST_METHOD"] == "GET") {
+
+            echo json_encode("SVAR GET");
+
+            exit; 
+
+
+        }
+
+    }
+
+
+} catch (exeption $err) {
+
+
+
+
+}
 
 
 
--- API/updateHoroscope.php
+++ API/updateHoroscope.php
@@ -1,16 +1,36 @@
 <?php
 
 
+try {
+
+    require("../horoscopeList.php"); 
+
+    if($_SERVER["REQUEST_METHOD"]) {
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
+            $horoscopeList = json_decode($_POST["body"], true);
+
+            error_log(json_encode($horoscopeList));
+
+            echo json_encode($_SESSION["name"]);
+
+            exit; 
 
 
+        }
+
+    } else {
+        $_SESSION[]
+    }
 
 
+} catch (exeption $err) {
 
+    
 
-
-
+}
 
 
 
