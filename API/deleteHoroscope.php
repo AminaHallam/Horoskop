21e650b98763cc549a41901ada5180811fb4f9bb
Amina Hallam <amina.hallam@gmail.com>
2021-12-14 12:03:01 +0100

    Create DELETE-endPoint

--- API/deleteHoroscope.php
+++ API/deleteHoroscope.php
@@ -7,16 +7,21 @@ try {
 
         if($_SERVER["REQUEST_METHOD"] == "DELETE") {
             
+            if(isset($_SESSION['zodiac'])) {
+                $session_destroy;
+
+                
+            }; 
+            
             
             echo json_encode('Delete funkar'); 
-           
 
             exit; 
 
 
-        } 
+        };
 
-    }
+    };
 
 
 } catch (exeption $err) {
@@ -24,7 +29,7 @@ try {
 
 
 
-}
+};
 
 
 
